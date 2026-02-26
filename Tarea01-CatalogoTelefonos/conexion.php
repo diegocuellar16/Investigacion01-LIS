e1be3352c37d9038a74ddc6e80f9d28c9d066420
diegocuellar16 <68791490+diegocuellar16@users.noreply.github.com>
2026-02-25 18:57:48 -0600

    Update conexion.php

--- Tarea01-CatalogoTelefonos/conexion.php
+++ Tarea01-CatalogoTelefonos/conexion.php
@@ -1,14 +1,15 @@
 <?php
 $servidor = "localhost";
-$usuario = "root"; // Tu usuario de XAMPP/WAMP
-$password = ""; // Normalmente en XAMPP va vacío
-$basededatos = "tienda_telefonos"; // ¡Asegúrate de que esta línea exista!
+$usuario = "root"; 
+$password = ""; 
+$basededatos = "tienda_telefonos"; 
 
-// Crear la conexión
+
 $conn = new mysqli($servidor, $usuario, $password, $basededatos);
 
-// Comprobar la conexión
+
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
 ?>
+
